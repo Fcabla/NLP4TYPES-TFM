98f641b587566984b1686ddbc09dcb4cf799633e
Fcabla <fer_nando_cb@hotmail.com>
2021-06-04 17:10:30 +0200

    Config parameters are in a new file

--- code/R/gold_standard.R
+++ code/R/gold_standard.R
@@ -1,12 +1,5 @@
-
-GS1_path <- "datasets/goldstandard/GS1.nt"
-GS2_path <- "datasets/goldstandard/GS2.nt"
-GS3_path <- "datasets/goldstandard/GS3.nt"
-column_names <- c("individual","property","type", "dot")
-#"models/model1m.rds"
-#"models/model10k.rds"
-#"models/model_full.rds"
-model_path <- "models/model10k.rds"
+# Read config parameters:
+source("code/R/ontology_trees.R")
 
 # Read data
 gold_standard <- read_TTL_file(GS1_path, column_names)
@@ -15,8 +8,9 @@ gold_standard <- rbind(gold_standard, read_TTL_file(GS3_path, column_names))
 df <- read_merge_TTL_files(abs_file_path, types_file_path, abs_col_names, typ_col_names, join_by)
 df <- subset(df, select = -type)
 df_gs <- merge(gold_standard, df, by="individual")
-df_gs <- remove_resources_url(df_gs, c("individual", "type"))
-
+if(remove_URL){
+  df_gs <- remove_resources_url(df_gs, c("individual", "type"))
+}
 
 # Read ontology
 dbo_tree <- get_tree_from_ontology(resourc = ont_path)
@@ -29,8 +23,8 @@ model <- readRDS(model_path)
 # vectorizate
 tdm <- vectorizate_dataframe(df_gs, tfidf=use_tfidf)
 tdm <- dfm_replace(tdm, pattern = "Bias", replacement = "bias")
-
 tdm <- dfm_match(tdm, featnames(model$x))
+
 # evaluate
 predicted <- predict_abstracts(model, tdm)
 metrics <- evaluate_results_dict(predicted, tdm$type, path_types_dic)
--- code/R/main.R
+++ code/R/main.R
@@ -6,62 +6,10 @@ source("code/R/text_preprocess_vectorization_q.R")
 source("code/R/classifiers.R")
 source("code/R/measurements.R")
 source("code/R/ontology_trees.R")
-
 library(data.table)
- 
-#####################
-# Config parameters #
-#####################
-# en or es
-lang <- "en"
-print(paste("Using language:",lang,", please check the paths and services are according to the lang selected"))
-# Files location and column names
-#abs_file_path <- "datasets/en/long_abstracts_en.ttl"
-#types_file_path <- "datasets/en/instance_types_en.ttl"
-abs_file_path <- paste("datasets/", lang,"/long_abstracts_",lang,".ttl",sep = "")
-types_file_path <- paste("datasets/", lang,"/instance_types_",lang,".ttl",sep = "")
-abs_col_names <- c("individual","property","abstract", "dot")
-typ_col_names <- c("individual","property","type", "dot")
-join_by <- "individual"
-use_sampled_df <- TRUE
-remove_URL <- TRUE
-# 1 -> 3M, 0.33 -> 1M, 0.0033 -> 10k
-sample_percentage <- 0.33
 
-# Name entity
-use_ne <- TRUE
-use_only_ne <- FALSE  
-confidence_lvl <- 0.33
-service_url <- DBPEDIA_PATH <- "http://localhost:2222/rest/annotate"
-#path_dict_ne <- "datasets/en/types_dict_en.csv"
-path_dict_ne <- paste("datasets/", lang,"/types_dict_",lang,".csv",sep = "")
-use_only_dict <- TRUE  # To use if the dict contains every individual
-dbo_only <- TRUE
-use_ne_path2root <- TRUE
-use_printable_names <- TRUE
-
-# text preprocessing and vectorization
-use_preprocessing <- FALSE
-use_stw <-TRUE
-custom_stw <- c("@en", "\"@en", "@es", "\"@es")
-use_stem <- TRUE
-use_lemm <- FALSE  
-remove_punctuation <- FALSE # Check if original does it
-use_tfidf <- TRUE
-
-# Classification
-train_percent <- 0.8
-use_crossval <- FALSE
-crossval_folds <- 5
-
-# Ontology trees
-ont_url <- "http://mappings.dbpedia.org/server/ontology/dbpedia.owl"
-ont_path <- "datasets/dbpedia_2016-10.owl"
-path_printable_names <- paste("datasets/", lang,"/printable_names_",lang,".csv",sep = "")
-# Measurements
-
-# Testing
-test_text <- "Animalia is an illustrated children's book by Graeme Base. It was originally published in 1986, followed by a tenth anniversary edition in 1996, and a 25th anniversary edition in 2012. Over three million copies have been sold. A special numbered and signed anniversary edition was also published in 1996, with an embossed gold jacket."
+# Read config parameters:
+source("code/R/ontology_trees.R")
 
 ##################
 # Start workflow #
